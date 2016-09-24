cd2a81df2d9843cd46a9a14fc26561d8fc6f0e9a
sandeep1988 <schourey1988@gmail.com>
2016-09-25 03:03:01 +0530

    Add Chnages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,8 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-  Paperclip.options[:command_path] = "/usr/local/bin/"
+  Paperclip.options[:command_path] = "C:\Program Files (x86)\GnuWin32\bin; C:\Program Files\ImageMagick-6.9.2-Q16"
+  Paperclip.options[:swallow_stderr] = false
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
