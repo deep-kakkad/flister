2b6437262c860ba188755de7c22d8541ac671e2a
sandeep1988 <schourey1988@gmail.com>
2016-09-25 17:40:09 +0530

    Add image in posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
   # has_attached_file :profile_image, styles: { medium: "300x300>", thumb: "500x500>" }, default_url: "/images/:style/missing.png"
-  has_attached_file :profile_image, styles: { medium: "200x200>", thumb: "500x500>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :profile_image, styles: { medium: "150x150>", thumb: "500x500>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :profile_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   validates :profile_image, attachment_presence: true
 end
